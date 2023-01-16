947414e5c8c2b5024b875ae63005aaf97bba47a9
Amel Khiri <53347966+algerina@users.noreply.github.com>
2023-01-16 23:12:25 +0100

    Add resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,35 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/edit'
-  get 'categories/delete'
+root "main#index"
 
-  # route "main#index"
 
   match 'about', to: "main#about", via: :get
   match 'hello', to: "main#hello", via: :get
 
   get 'main/index'
 
+  resources :categories  do
+    member do
+      get :delete
+    end
+  end
+
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+
   get 'tasks/index'
   get 'tasks/new'
   get 'tasks/edit'
   get 'tasks/show'
   get 'tasks/delete'
+
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/delete'
+
  
 end
