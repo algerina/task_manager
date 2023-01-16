aefe570f9fbf969bd26c80c2af1860336f03e262
Amel Khiri <53347966+algerina@users.noreply.github.com>
2023-01-16 21:12:18 +0100

    Update tasks routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,11 @@ Rails.application.routes.draw do
   match 'hello', to: "main#hello", via: :get
 
   get 'main/index'
+
   get 'tasks/index'
   get 'tasks/new'
   get 'tasks/edit'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'tasks/show'
+  get 'tasks/delete'
+ 
 end
