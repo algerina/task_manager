09c4bbdc6043460cba129812a9478dbf28a153a6
Amel Khiri <53347966+algerina@users.noreply.github.com>
2023-01-19 23:14:57 +0100

    Create edit action for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,9 +22,18 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+
   end
 
   def update
+  @task = Task.find(params[:id])
+  if @task.update(task_params)
+    redirect_to task_path(@task)
+  else
+    render ('edit')
+  
+  end
   end
 
   def delete
