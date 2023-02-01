f694d00ffe376e55d7712e7ab2007aec5ff8da06
Amel Khiri <53347966+algerina@users.noreply.github.com>
2023-02-01 22:06:17 +0100

    Correct scopes syntax errors

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :category, optional: true
 
-  scope :complete, -> { where(:completed: true) }
-  scope :incomplete, -> { where(:completed: false) }
-  scope :sorted, -> { order(:last_name, :first_name) }
+  scope :complete, -> { where(completed: true) }
+  scope :incomplete, -> { where(completed: false) }
+  scope :sorted, -> { order(:position) }
   scope :search, -> (kw) { where("LOWER(name) LIKE ?", "%#{kw.downcase}%") }
 
 
